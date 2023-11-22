8185369308c6e4636ebf7c6121d3d1c67b52d81b
universalshit <151498881+universalshit@users.noreply.github.com>
2023-11-22 12:40:53 +0530

    Update CCCC.py

--- CCCC.py
+++ CCCC.py
@@ -245,17 +245,44 @@ In conclusion, cryptocurrency represents a transformative force in the world of 
             break
     elif ch == '7':
         print("EDITING DATABASE - DEVESH")
-        
-       # D
-       # E
-       # V
-       # E
-       # S
-       # H
-        
-        
-    
-        
+
+    # D E V E S H 
+
+    import turtle
+import time
+
+# Create screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+# Create nucleus
+nucleus = turtle.Turtle()
+nucleus.shape("circle")
+nucleus.color("yellow")
+
+# Create orbital
+orbital = turtle.Turtle()
+orbital.color("blue")
+orbital.penup()
+orbital.goto(0, -100)
+orbital.pendown()
+orbital.circle(100)
+
+# Create electron
+electron = turtle.Turtle()
+electron.shape("circle")
+electron.color("white")
+electron.penup()
+electron.goto(0, -100)
+
+# Move electron in orbital
+while True:
+    electron.circle(100, 10)
+    time.sleep(0.1)
+
+# This will keep the screen open until it is clicked
+turtle.done()
+       
         
     else:
         print('INVALID INPUT ERROR')
